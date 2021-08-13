342baf285805cd811438696ef289089225625ac6
Praveen <sankar_praveen@hotmail.com>
2021-08-13 16:05:35 +0530

    Add http service to login and fetch all staff

---
+++ FrontEnd/Angular/Staff-Dashboard/src/app/http.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Login } from './Modals/Login';
+import { Staff } from './Modals/Staff';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  
+  constructor(private http: HttpClient) { 
+    if(localStorage.getItem("TOKEN")==null){
+      this.Login({userName: "Praveen Sankar", password : "new password"});
+    }
+  }
+
+  Login(login : Login){
+    this.http.post("https://localhost:5001/api/account/logon", login).subscribe(res=> localStorage.setItem("TOKEN", res.toString()));
+  }
+
+  FetchAllStaff(){
+    return this.http.get<Staff[]>("https://localhost:5001/api/staff");
+  }
+}
